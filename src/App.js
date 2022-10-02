782740c8b8b1640248a35215e1e3c0a8e7481157
JainPriya1234 <priya.jain11302@gmail.com>
2022-10-02 21:24:34 +0530

    Todo commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import Todoitem from "./Todoitem";
+import InputArea from "./InputArea";
+function App() {
+    
+    const[items, setitems] = useState([ ]);
+   
+  function addItem(count){
+    setitems((prevItems) => {
+          return [...prevItems ,count ];
+    })
+   
+ }
+
+ function deleteItem(id){
+     setitems((prevItems) => {
+         return prevItems.filter(
+            (item , index)=>{
+                return index !==  id
+            }
+         )
+     })
+ }
+return (
+    <div className="container">
+    <div className="heading">
+      <h1>To-Do List</h1>
+    </div>
+     <InputArea 
+     onAdd ={addItem}
+     /> 
+    <div>
+        <ul>
+            {
+                items.map((todoItem , index) => (
+                    <Todoitem 
+                    key={index}
+                    id={index}
+                    text = {todoItem}
+                    onChecked ={deleteItem}
+                    />
+                ))
+            }
+        </ul>
+    </div>
+
+  </div>
+);
+}
+
+export default App;
---
+++ src/InputArea.jsx
@@ -0,0 +1,25 @@
+import React , {useState} from 'react'
+
+function InputArea(props){
+    const[count,setcount]= useState(" ");
+  
+    function handleChange(event){
+        const newValue = event.target.value;
+         setcount(newValue);
+      }
+  return (
+    <div className="form">
+    <input type="text"
+     onChange={handleChange} 
+     value={count} />
+    <button onClick={() => {
+        props.onAdd(count);
+        setcount(" ");
+    }}>
+      <span>Add</span>
+    </button>
+  </div>
+  );
+}
+
+export default InputArea
