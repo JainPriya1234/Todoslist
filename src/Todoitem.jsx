8e04337e787bd98bbe63296026debac58c13b2f6
JainPriya1234 <91417671+JainPriya1234@users.noreply.github.com>
2022-10-03 13:39:36 +0530

    Update Todoitem.jsx

--- src/Todoitem.jsx
+++ src/Todoitem.jsx
@@ -1,16 +1,19 @@
-import React  from 'react'
+import React from 'react'
 
-function Todoitem (props) {
-   
-  return (
-    <div onClick={() =>
-    {
-        props.onChecked(props.id);
-    }}>
-      <li> {props.text}</li>
-      </div>
-    
-  )
+function Todoitem(props) {
+
+    return (
+        <div className='list' >
+            <span><li > {props.text}</li></span>
+         <span className='button' >  <button    class="btn btn-danger  btn btn-sml "
+                onClick={() => {
+                    props.onChecked(props.id);
+                }}> Delete
+            </button></span>
+
+        </div>
+
+    )
 }
 
 export default Todoitem
